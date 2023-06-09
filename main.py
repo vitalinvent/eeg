78603576dfa610623cfa19f8f8baa1d1b6a3f5bb
vitalinvent <vitalinvent@gmail.com>
2023-06-09 16:01:48 +0300

    eeg working

---
+++ main.py
@@ -0,0 +1,35 @@
+import serial
+import time
+import matplotlib.pyplot as plt
+
+start = time.time()
+bufferSize = 50
+x = [0]*bufferSize
+y = [0]*bufferSize
+xi = 0
+# x = []
+# y = []
+
+ser = serial.Serial('COM3')
+time.sleep(2)
+fig = plt.figure()
+plt.ion()  # turn on interactive mode
+fig.canvas.draw()
+plt.show(block=False)
+
+while True:
+    num = int.from_bytes(ser.read(), "big")
+    end = time.time()
+    # y.append(num)
+    y[xi] = num
+    time_elapsed= end - start
+    # x.append(time_elapsed)
+    x[xi] = xi
+    # x[xi] = xi
+    plt.cla()
+    plt.plot(x, y, 'red')
+    plt.pause(0.05)
+    plt.draw()
+    xi += 1
+    if xi > bufferSize-1:
+        xi = 0
